fa4ebd01bd521eda4b2a109b1d6764467c0199a4
Kris Kamikawa <kris.kamikawa@gmail.com>
2019-08-18 21:41:12 -0600

    add example of adding constraint to generic

--- generics.ts
+++ generics.ts
@@ -22,4 +22,60 @@ class ArrayOfAnything<T> {
   }
 }
 
-new ArrayOfAnything<string>(["a", "b", "c"]);
+new ArrayOfAnything(["a", "b", "c"]);
+
+//Example of generics with functions
+
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printAnything<T>(arr: T[]) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything<string>(["a", "b", "c"]);
+//this is also fine, uses type inference, but it's better to add type annotation for generics
+printAnything([1, 2, 3]);
+
+// Generic Constraints
+
+class Car {
+  print() {
+    console.log("I am a car");
+  }
+}
+
+class House {
+  print() {
+    console.log("I am a house");
+  }
+}
+
+//add constraint that promises there will be a print method
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+printHousesOrCars<House>([new House(), new House()]);
+printHousesOrCars<Car>([new Car(), new Car()]);
+
+// Error, numbers do not satisfy type Printable
+// printHousesOrCars([1, 2, 3, 4, 5]);
